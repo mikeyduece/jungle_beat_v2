bb2fe6c6d679af640754b1f2638716140a98402b
Mike Heft <mikeheft@gmail.com>
2017-06-29 16:35:32 -0600

    Node passing

--- test/node_test.rb
+++ test/node_test.rb
@@ -9,8 +9,13 @@ class NodeTest < Minitest::Test
   end
 
   def test_can_hold_data
-    node =Node.new("plop")
+    node = Node.new("plop")
     assert_equal "plop", node.data
+  end
+
+  def test_next_node_is_nil
+    node = Node.new
+    assert_nil node.next_node
   end
 
 end
