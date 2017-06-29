10a396e5b7210e18b151c79cc39058c5fd988aef
Mike Heft <mikeheft@gmail.com>
2017-06-29 17:29:32 -0600

    append done

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,30 @@
 require './lib/node'
 
 class LinkedList
+  attr_reader :head
+
+  def intialize
+    @head = nil
+  end
+
+  def append(sound)
+    if head.nil?
+      @head = Node.new(sound)
+    else
+      current_node = @head
+      until current_node.next_node == nil
+        current_node = current_node.next_node
+      end
+      current_node.next_node = Node.new(sound)
+    end
+    sound
+  end
 
 end
+
+
+
+# > list.count
+# => 1
+# > list.to_string
+# => "doop"
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -2,10 +2,29 @@ require './test/test_helper'
 require './lib/linked_list'
 
 class LinkedListTest < Minitest::Test
+  attr_reader :list
+
+  def setup
+    @list = LinkedList.new
+  end
 
   def test_it_exists
-    list = LinkedList.new
     assert_instance_of LinkedList, list
   end
+
+  def test_head_is_nil_by_default
+    assert_nil list.head
+  end
+
+  def test_it_can_append
+    list.append("doop")
+    assert_equal "doop", list.head.data
+    list.append("wha")
+    assert_equal "wha", list.head.next_node.data
+    list.append("wha")
+    assert_equal "wha", list.head.next_node.next_node.data
+    assert_nil list.head.next_node.next_node.next_node
+  end
+
 
 end
